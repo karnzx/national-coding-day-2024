2dbee2ebbf09ec999b7df89783eb4454eaa22526
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-25 20:57:30 +0700

    jsapp testing

---
+++ jsapp/app/fizzbuzz.test.ts
@@ -0,0 +1,8 @@
+import { FizzBuzz } from './fizzbuzz'
+
+describe('Taxi', () => {
+  it('example test', () => {
+    const result = FizzBuzz()
+    expect(result).toEqual(55)
+  })
+})
---
+++ jsapp/app/fizzbuzz.ts
@@ -0,0 +1,1 @@
+export const FizzBuzz = () => 55
