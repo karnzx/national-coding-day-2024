521085f6afb662d0fae2e9471d0972822fa3d6fe
Piravit Chenpittaya <piravit.chenpittaya@gmail.com>
2024-11-29 10:40:46 +0700

    feat product add spy test

--- jsapp/app/product.test.ts
+++ jsapp/app/product.test.ts
@@ -3,10 +3,26 @@ import { getProductPrice, Product } from "./product";
 function expect(actual: any) {
   return {
     toEqual: (expected: any) => {
-      if (actual === expected) {
+      if (JSON.stringify(actual) === JSON.stringify(expected)) {
         console.log(" ✅ PASS");
       } else {
         console.log(` ❌ FAIL: want ${expected} but got ${actual}`);
+      }
+    },
+    toHaveBeenCalled: () => {
+      if (actual.called === 1) {
+        console.log(" ✅ PASS");
+      } else {
+        console.log(
+          ` ❌ FAIL: should call once but got called ${actual.called} times`
+        );
+      }
+    },
+    toHaveBeenCalledWith: (...args: any[]) => {
+      if (JSON.stringify(actual.args) === JSON.stringify(args)) {
+        console.log(" ✅ PASS");
+      } else {
+        console.log(` ❌ FAIL: want ${args} but got ${actual.args}`);
       }
     },
   };
@@ -94,3 +110,59 @@ test("Fake: get product not found", () => {
     expect(error.message).toEqual("Product not found");
   }
 });
+
+// Spy: Keeo track of the call made to the function
+// Dynamic language need to check for function parameter type too.
+
+function spyOn(obj: any, method: string = "search") {
+  let spy = {
+    called: 0,
+    args: [],
+  };
+
+  const originalMethod = obj[method];
+
+  spy[method] = (...args: any[]) => {
+    spy.called++;
+    spy.args = args;
+    return originalMethod(...args);
+  };
+
+  return spy;
+}
+
+test("Spy: search should have been called once", () => {
+  const stub = {
+    search: (productName: string, productId: string) => {
+      return { productName, productId, price: 999.99 };
+    },
+  };
+
+  const spy = spyOn(stub, "search");
+
+  const actual = getProductPrice(spy as any, "Laptop", "LAPTOP-123");
+
+  expect(actual).toEqual(999.99);
+  expect(spy).toHaveBeenCalled();
+});
+
+test("Spy: should call search with productName and productId", () => {
+  const stub = {
+    search: (productName: string, productId: string) => {
+      return { productName, productId, price: 999.99 };
+    },
+  };
+
+  const spy = spyOn(stub, "search");
+
+  const actual = getProductPrice(spy as any, "Laptop", "LAPTOP-123");
+
+  expect(actual).toEqual(999.99);
+  expect(spy).toHaveBeenCalledWith("Laptop", "LAPTOP-123");
+});
+
+// Mocks: are pre-programmed with expectation which from a specification
+// Mocks are used to define expectations about function calls.
+// Spies are used to track function call without pre-setting expection
+// Mocks can be use a lot in every test case but the test with take too much memory so
+// use the right tool for the right test
